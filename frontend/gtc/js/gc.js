78d66058cdffecffe4cd05127bf4daab745796de
DiabolicKitten <git@cartelera.org>
2017-08-22 19:25:51 +0300

    add json parser

--- frontend/gtc/js/gc.js
+++ frontend/gtc/js/gc.js
@@ -1,6 +1,14 @@
 console.log('contracts');
-$.get( "test.json", function( data ) {
-  global tmp;
-  tmp=data;
-  alert( "Load was performed." );
-},function(data){alert( "ne zagruzilo." )},'json');
+$.get( "http://127.0.0.1:3000/test.json", function( data ) {
+  console.log(1);
+  console.log(data);
+  data.forEach(function(item, i, arr) {
+  let a;
+  a = parseInt($("#contracts").find("tr").last().find('td').first().text())+1;
+  a=''+a;
+  console.log(a)
+  $('#contracts tr:last').after('<tr><td>'+a+'</td><td>'+item.name+'</td><td>'+item.discription+'</td><td>'+item.price+'</td><td><button type="button" class="btn btn-success btn-md show" id="go">Отправить решение</button> </td></tr>')
+
+});
+  
+});
