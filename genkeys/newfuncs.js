93eaa91ef19cd1972fb4c97e24f37b86232578d5
rylm <danilov.sasha444sch@gmail.com>
2017-08-22 21:44:40 +0300

    Add test func

--- genkeys/newfuncs.js
+++ genkeys/newfuncs.js
@@ -176,3 +176,60 @@ function vote(keys, block, vote_status) {
 
 	}
 }
+
+function testCreateScholarship(keys, reward, task_info) {
+	var TxFullScholarshipT = Exonum.newType({
+	    size: 64,
+	    fields: {
+	        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+	        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+	        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+	        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+	        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+	    }
+	});
+
+	var data = {
+			reward: reward,
+			task_info: task_info,
+			pub_key: keys.publicKey,
+			signer_info: '', 
+			vote_status: 0
+	};
+
+	signature = Exonum.sign(keys.secretKey, data, TxFullScholarshipT);
+
+	fullScholarship = Exonum.newMessage({
+		size: 64,
+		network_id: 0,
+		protocol_version: 0,
+		service_id: 1,
+		message_id: 0,
+		signature: signature,
+		fields: {
+	        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+	        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+	        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+	        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+	        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+	    }
+	});
+
+	signature2 = fullScholarship.sign(keys.secretKey, data);
+
+	return {
+		"body": {
+	    "reward": reward,
+	    "task_info": task_info,
+	    "pub_key": keys.publicKey,
+	    "signer_info": '',
+	    "vote_status": 0
+	  	},
+	  	"network_id": 0,
+	  	"protocol_version": 0,
+	  	"service_id": 1,
+	  	"message_id": 0,
+	  	"signature": signature2
+	}
+
+}
