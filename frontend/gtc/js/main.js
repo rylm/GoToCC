313653ed92bb496b54d5000c09f26f848641e46e
DiabolicKitten <git@cartelera.org>
2017-08-28 12:28:48 +0300

    add pos func

---
+++ frontend/gtc/js/main.js
@@ -0,0 +1,40 @@
+//function genKey(){
+ //let tmp =Exonum.keyPair()
+//
+
+//}
+Cookies.set('publicKey', "02b9c6561322f68d2cf973e8d544d91716bf0b04874914f31dcde6de99c5c9a1");
+Cookies.set('secretKey', "6ba5966eddacd09563868ea0ac21f9efdb82e6c86ce0a252d02d0f62c026ae6a02b9c6561322f68d2cf973e8d544d91716bf0b04874914f31dcde6de99c5c9a1");
+console.log(Cookies.get());
+$(document).ready(function() { // вся мaгия пoсле зaгрузки стрaницы
+	$('button#newContract').click( function(event){
+  console.log('ffff');
+  var tmp=setup(Cookies.get(),$('#prCoast').val() ,$('#prName').val());
+  console.log(tmp);
+   var kk='{  "body":    "reward":"100",    "task_info":"123",    "pub_key":"ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff",    "signer_info":"",    "vote_status":"0"  },  "network_id":0,  "protocol_version":0,  "service_id":1,  "message_id":0,  "signature":"92d593f6d514b533fab01dd3c5ac2d590474674c4d2880c8fb02bee6d58a26e34315abc5e79d634cec6f34b5e216614a11a65d86708a3ac4bff537fca1c9c20e"}'
+  if(tmp!=undefined){
+    console.log('zap');
+    function xuy(data){
+      console.log(data);
+      alert(data);
+    }
+    $.post({url :'http://google.com',data : tmp, success:xuy()});
+    $.ajax({
+      url:"http://google.com",
+      type:"POST",
+      headers: {
+
+      },
+      data:kk,
+      dataType:"json"})
+
+
+  }
+
+
+
+  }
+)
+
+}
+);
---
+++ frontend/gtc/js/newfuncs.js
@@ -0,0 +1,178 @@
+function setup(keys, reward, task_info) {
+	var keyPair = keys;
+
+	if (keyPair.publicKey == "02b9c6561322f68d2cf973e8d544d91716bf0b04874914f31dcde6de99c5c9a1") {
+		var TxFullScholarshipT = Exonum.newType({
+		    size: 64,
+		    fields: {
+		        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+		        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+		        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+		        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+		        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+		    }
+		});
+
+		var dpk = 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
+
+		var data = {
+			reward: reward,
+			task_info: task_info,
+			pub_key: dpk,
+			signer_info: ' ',
+			vote_status: 0
+		};
+
+		signature = Exonum.sign(keyPair.secretKey, data, TxFullScholarshipT);
+
+		fullScholarship = Exonum.newMessage({
+			size: 64,
+			network_id: 0,
+			protocol_version: 0,
+			service_id: 1,
+			message_id: 0,
+			signature: signature,
+			fields: {
+		        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+		        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+		        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+		        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+		        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+		    }
+		});
+
+		signature2 = fullScholarship.sign(keyPair.secretKey, data);
+
+		return {
+			"body": {
+		    "reward": reward + '',
+		    "task_info": task_info,
+		    "pub_key": dpk,
+		    "signer_info": ' ',
+		    "vote_status": 0 + ''
+		  	},
+		  	"network_id": 0,
+		  	"protocol_version": 0,
+		  	"service_id": 1,
+		  	"message_id": 0,
+		  	"signature": signature2
+		}
+
+	}
+}
+
+function assign(keys, signer_info, block) {
+	var TxFullScholarshipT = Exonum.newType({
+		    size: 64,
+		    fields: {
+		        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+		        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+		        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+		        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+		        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+		    }
+		});
+
+		var data = {
+			reward: block.reward,
+			task_info: block.task_info,
+			pub_key: keyPair.publicKey,
+			signer_info: signer_info,
+			vote_status: 0
+		};
+
+		signature = Exonum.sign(keyPair.secretKey, data, TxFullScholarshipT);
+
+		fullScholarship = Exonum.newMessage({
+			size: 64,
+			network_id: 0,
+			protocol_version: 0,
+			service_id: 1,
+			message_id: 0,
+			signature: signature,
+			fields: {
+		        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+		        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+		        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+		        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+		        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+		    }
+		});
+
+		signature2 = fullScholarship.sign(keyPair.secretKey, data);
+
+		return {
+			"body": {
+		    "reward": block.reward + '',
+		    "task_info": block.task_info,
+		    "pub_key": keyPair.publicKey,
+		    "signer_info": signer_info,
+		    "vote_status": 0 + ''
+		  	},
+		  	"network_id": 0,
+		  	"protocol_version": 0,
+		  	"service_id": 1,
+		  	"message_id": 0,
+		  	"signature": signature2
+		}
+
+}
+
+function vote(keys, block, vote_status) {
+	if (keyPair.publicKey == '9cd129929c2b5afb7f7e7320ce289e26ae9020e9a03a44b88095b5798632e6b1') {
+		var TxFullScholarshipT = Exonum.newType({
+		    size: 64,
+		    fields: {
+		        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+		        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+		        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+		        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+		        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+		    }
+		});
+
+		var data = {
+			reward: block.reward,
+			task_info: block.task_info,
+			pub_key: keyPair.publicKey,
+			signer_info: block.signer_info,
+			vote_status: vote_status
+		};
+
+		signature = Exonum.sign(keyPair.secretKey, data, TxFullScholarshipT);
+
+		fullScholarship = Exonum.newMessage({
+			size: 64,
+			network_id: 0,
+			protocol_version: 0,
+			service_id: 1,
+			message_id: 0,
+			signature: signature,
+			fields: {
+		        reward: {type: Exonum.Int64, size: 8, from: 0, to: 8},
+		        task_info: {type: Exonum.String, size: 8, from: 8, to: 16},
+		        pub_key: {type: Exonum.PublicKey, size: 32, from: 16, to: 48},
+		        signer_info: {type: Exonum.String, size: 8, from: 48, to: 56},
+		        vote_status: {type: Exonum.Int64, size: 8, from: 56, to: 64}
+		    }
+		});
+
+		signature2 = fullScholarship.sign(keyPair.secretKey, data);
+
+		return {
+			"body": {
+		    "reward": block.reward + '',
+		    "task_info": block.task_info,
+		    "pub_key": keyPair.publicKey,
+		    "signer_info": block.signer_info,
+		    "vote_status": vote_status + ''
+		  	},
+		  	"network_id": 0,
+		  	"protocol_version": 0,
+		  	"service_id": 1,
+		  	"message_id": 0,
+		  	"signature": signature2
+		}
+
+	}
+}
